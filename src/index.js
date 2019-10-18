e5571361b1b3f4fed13964b7d39677cf74d7f668
Erik Pantzar <erik@epantzar.se>
2019-10-19 01:50:36 +0200

    Update data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-console.log('hej')
+window.state = {
+    players: [],
+    stats: []
+}
 
-fetch('./data/players.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+async function initalizeData() {
+    const players = await fetch('./data/players.json')
+        .then(response => response.json())
+        .then(data => {
+            return data.players.map(p => {
+                const { nameOnPlatform, profileId } = p
+                return {
+                    nameOnPlatform, profileId
+                }
+            })
+        })
 
-fetch('./data/response.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+    const stats = await fetch('./data/response.json')
+        .then(response => response.json())
+        .then(data => data)
+
+    const labels = await fetch('./data/labels.json')
+        .then(response => response.json())
+        .then(data => data)
+
+    
+
+    console.log(players, stats, labels)
+}
+
+
+initalizeData()
