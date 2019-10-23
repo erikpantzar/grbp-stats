bf0149b83009b835c9a8dab16f8e56cdc130c673
Erik Pantzar <erik@epantzar.se>
2019-10-23 22:00:23 +0200

    update stats

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ async function initalizeData() {
       })
     })
 
-  const stats = await fetch('./data/response221.json')
+  const stats = await fetch('./data/response230.json')
     .then(response => response.json())
     .then(data => {
       const profiles = data.profiles.map(profile => {
